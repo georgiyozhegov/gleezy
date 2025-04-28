8ce23bd5aa1873c0ed93c4ed3e4c4cf031ff0939
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-04-28 21:20:18 +0400

    add while loop and assignment statement support
    - Add Assign and While statements
    - Refactor Let statement to use Assign internally
    - Update syntax specification

--- lib/gleezy-parse/src/statement.rs
+++ lib/gleezy-parse/src/statement.rs
@@ -5,12 +5,16 @@ use crate::{Expression, Parsable, Parse};
 #[derive(Debug)]
 pub enum Statement {
     Let(Let),
+    Assign(Assign),
+    While(While),
 }
 
 impl Parsable for Statement {
     fn parse(source: &mut Parse) -> Self {
         match source.peek().expect("expected statement").kind() {
             TokenKind::Let => Self::Let(Let::parse(source)),
+            TokenKind::Identifier(..) => Self::Assign(Assign::parse(source)),
+            TokenKind::While => Self::While(While::parse(source)),
             _ => panic!("expected statement"),
         }
     }
@@ -18,11 +22,23 @@ impl Parsable for Statement {
 
 #[derive(Debug)]
 pub struct Let {
+    assign: Assign,
+}
+
+impl Parsable for Let {
+    fn parse(source: &mut Parse) -> Self {
+        let assign = Assign::parse(source);
+        Self { assign }
+    }
+}
+
+#[derive(Debug)]
+pub struct Assign {
     name: Identifier,
     value: Expression,
 }
 
-impl Parsable for Let {
+impl Parsable for Assign {
     fn parse(source: &mut Parse) -> Self {
         source.eat(TokenKind::Let);
         let name = Identifier::parse(source);
@@ -43,5 +59,40 @@ impl Parsable for Identifier {
             TokenKind::Identifier(name) => Self { name },
             _ => panic!("expected identifier"),
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct While {
+    condition: Expression,
+    body: Body,
+}
+
+impl Parsable for While {
+    fn parse(source: &mut Parse) -> Self {
+        source.eat(TokenKind::While);
+        let condition = Expression::parse(source);
+        source.eat(TokenKind::Do);
+        let body = Body::parse(source);
+        source.eat(TokenKind::End);
+        Self { condition, body }
+    }
+}
+
+#[derive(Debug)]
+pub struct Body {
+    inner: Vec<Statement>,
+}
+
+impl Parsable for Body {
+    fn parse(source: &mut Parse) -> Self {
+        let mut inner = Vec::new();
+
+        while source.peek().is_some_and(|token| token.kind() != &TokenKind::End) {
+            let statement = Statement::parse(source);
+            inner.push(statement);
+        }
+
+        Self { inner }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ fn main() {
     let source = r#"
 let name = 2
 let bruh = 4
+while 1 do
+    let a = 1
+    let b = 2
+end
 "#;
     let source = source.chars().peekable();
     let lex = Lex::new(source);
     let parse = Parse::new(lex.peekable());
     for statement in parse {
-        println!("{statement:?}");
+        println!("{statement:#?}");
     }
 }
